3a090264c229ad260584f3043467f71c5c4a94ac
Aaron Frantisak <afrantisak@gmail.com>
2016-01-28 07:26:46 -0600

    refactor out new_user and new_message

--- server.py
+++ server.py
@@ -4,20 +4,31 @@ from eventlet.green import socket
 PORT = 3001
 participants = set()
 
+def new_user(connection, address):
+    print("Participant joined chat.")
+    new_writer = new_connection.makefile('w')
+    new_writer.write('type your name:')
+    new_writer.flush()
+    name = new_writer.readline().strip()
+    return new_writer
+
+def new_message(writer, line, participants):
+    print("Chat:", line.strip())
+    for p in participants:
+        try:
+            if p is not writer:  # Don't echo
+                p.write(line)
+                p.flush()
+        except socket.error as e:
+            # ignore broken pipes, they just mean the participant
+            # closed its connection already
+            if e[0] != 32:
+                raise
+
 def read_chat_forever(writer, reader):
     line = reader.readline()
     while line:
-        print("Chat:", line.strip())
-        for p in participants:
-            try:
-                if p is not writer:  # Don't echo
-                    p.write(line)
-                    p.flush()
-            except socket.error as e:
-                # ignore broken pipes, they just mean the participant
-                # closed its connection already
-                if e[0] != 32:
-                    raise
+        new_message(writer, line, participants)
         line = reader.readline()
     participants.remove(writer)
     print("Participant left chat.")
@@ -27,11 +38,11 @@ try:
     server = eventlet.listen(('0.0.0.0', PORT))
     while True:
         new_connection, address = server.accept()
-        print("Participant joined chat.")
-        new_writer = new_connection.makefile('w')
+        new_writer = new_user(new_connection, address)
         participants.add(new_writer)
         eventlet.spawn_n(read_chat_forever,
                          new_writer,
                          new_connection.makefile('r'))
 except (KeyboardInterrupt, SystemExit):
     print("ChatServer exiting.")
+
